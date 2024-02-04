d4087da4f24cd8ab22ee81d9274748479bd18ceb
meangpu <51825872+meangpu@users.noreply.github.com>
2024-02-04 23:23:17 +0700

    add gacha count

---
+++ Runtime/Gacha/UI/GachaItemUILeftCount.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace Meangpu.Gacha
+{
+    public class GachaItemUILeftCount : MonoBehaviour
+    {
+        [SerializeField] TMP_Text _countTxt;
+        [SerializeField] GachaManager _gachaManagerScpt; // for update on start
+
+        void OnEnable() => ActionGacha.OnCurrentDictUpdate += OnGachaUpdate;
+        void OnDisable() => ActionGacha.OnCurrentDictUpdate -= OnGachaUpdate;
+
+        private void Start() => OnGachaUpdate(_gachaManagerScpt.NowDictData);
+
+        private void OnGachaUpdate(Dictionary<GameObject, int> dictionary) => UpdateCount(dictionary.Values.Sum());
+        public void UpdateCount(int newCount) => _countTxt.SetText(newCount.ToString());
+    }
+}
