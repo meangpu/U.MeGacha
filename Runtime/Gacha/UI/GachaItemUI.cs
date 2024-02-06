a7b28ee1179dfe066839cbfd21f232a5e3ed7dce
meangpu <51825872+meangpu@users.noreply.github.com>
2024-02-06 22:04:49 +0700

    stable gacha

--- Runtime/Gacha/UI/GachaItemUI.cs
+++ Runtime/Gacha/UI/GachaItemUI.cs
@@ -2,18 +2,22 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Meangpu.Gacha
 {
     public class GachaItemUI : MonoBehaviour
     {
+        [Header("Main data to check")]
+        public GameObject keyData;
+        [Header("display setting")]
         [SerializeField] protected TMP_Text _itemName;
         [SerializeField] protected TMP_Text _itemCount;
         [SerializeField] protected TMP_Text _itemGetPercent;
         [SerializeField] string _percentEndText = "%";
-        float _percentToGet;
+        [SerializeField] Slider _sliderPercent;
 
-        public GameObject keyData;
+        float _percentToGetAsZeroPointFloat;
 
         void OnEnable()
         {
@@ -31,12 +35,17 @@ namespace Meangpu.Gacha
                 UpdateCount(dictionary[keyData]);
             }
 
-            if (_itemGetPercent == null) return;
+            _percentToGetAsZeroPointFloat = dictionary[keyData] / (float)dictionary.Values.Sum();
+            if (float.IsNaN(_percentToGetAsZeroPointFloat)) _percentToGetAsZeroPointFloat = 0;
 
-            _percentToGet = dictionary[keyData] / (float)dictionary.Values.Sum();
-
-            if (float.IsNaN(_percentToGet)) _percentToGet = 0;
-            UpdatePercent(_percentToGet);
+            if (_itemGetPercent != null)
+            {
+                UpdatePercent(_percentToGetAsZeroPointFloat);
+            }
+            if (_sliderPercent != null)
+            {
+                UpdatePercentSlider(_percentToGetAsZeroPointFloat);
+            };
         }
 
         public virtual void InitUI(GameObject obj, int count)
@@ -45,9 +54,18 @@ namespace Meangpu.Gacha
             _itemName.SetText(obj.name);
             UpdateCount(count);
             UpdatePercent(0);
+            InitSlider();
+        }
+
+        void InitSlider()
+        {
+            _sliderPercent.minValue = 0;
+            _sliderPercent.maxValue = 100;
+            _sliderPercent.value = 0;
         }
 
         public void UpdateCount(int newCount) => _itemCount.SetText(newCount.ToString());
         public void UpdatePercent(float percent) => _itemGetPercent.SetText($"{percent * 100:F2}{_percentEndText}");
+        public void UpdatePercentSlider(float percent) => _sliderPercent.value = percent * 100;
     }
 }
