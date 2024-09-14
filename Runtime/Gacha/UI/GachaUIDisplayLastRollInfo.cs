b391f0886a01ca04c40f657d40949b3e14eafe28
meangpu <51825872+meangpu@users.noreply.github.com>
2024-09-14 23:32:17 +0700

    Update GachaUIDisplayLastRollInfo.cs

--- Runtime/Gacha/UI/GachaUIDisplayLastRollInfo.cs
+++ Runtime/Gacha/UI/GachaUIDisplayLastRollInfo.cs
@@ -5,8 +5,11 @@ namespace Meangpu.Gacha
 {
     public class GachaUIDisplayLastRollInfo : MonoBehaviour
     {
+        [Header("Percent")]
         [SerializeField] TMP_Text _textRollPercent;
-        [SerializeField] string _floatingPoint = "P2";
+        [SerializeField] string _floatingPoint = "F2";
+        [SerializeField] string _endingWord = "%";
+        [Header("Name")]
         [SerializeField] TMP_Text _itemName;
 
         void OnEnable()
@@ -21,8 +24,8 @@ namespace Meangpu.Gacha
 
         private void DisplayLastRollInfo(float percent, string objectName)
         {
-            string percentNumber = percent.ToString(_floatingPoint);
-            _textRollPercent?.SetText($"{percentNumber}");
+            string percentNumber = (percent * 100).ToString(_floatingPoint);
+            _textRollPercent?.SetText($"{percentNumber}{_endingWord}");
             _itemName?.SetText(objectName);
         }
 
